935b3a6b42ce702d4e67237ad82454ad98e6cfd4
R1shShah <148688518+R1shShah@users.noreply.github.com>
2024-06-14 14:43:30 +0530

    testing for errors

--- mysite/deployment.py
+++ mysite/deployment.py
@@ -5,8 +5,8 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
-
+# DEBUG = False
+DEBUG = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
